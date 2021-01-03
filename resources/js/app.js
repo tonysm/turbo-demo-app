a7c805696db23f7d880ec30fbb70cd67f82c7b3d
Tony Messias <tonysm@hey.com>
2021-01-02 23:56:28 -0300

    Installs turbo

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,12 @@
-require('./bootstrap');
+import {
+    start as startTurbo
+} from "@hotwired/turbo";
+import('./bootstrap');
+import('./turbo-echo-stream-tag');
+import { Application } from "stimulus"
+import { definitionsFromContext } from "@stimulus/webpack-helpers"
 
-require('alpinejs');
+startTurbo();
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
