9b3437598f92c49f5f8f2e9feb98c92731c5c351
Tony Messias <tonysm@hey.com>
2021-01-08 11:18:34 -0300

    Adds the livewire turbo adapter

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,5 +39,6 @@
         @stack('modals')
 
         @livewireScripts
+        <script src="https://cdn.jsdelivr.net/gh/livewire/turbolinks@v0.1.x/dist/livewire-turbolinks.js" data-turbolinks-eval="false" data-turbo-eval="false"></script>
     </body>
 </html>
