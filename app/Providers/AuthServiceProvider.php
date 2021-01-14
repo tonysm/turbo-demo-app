ddca83a6253bd626b94c16b4faf7564d890c935e
Tony Messias <tonysm@hey.com>
2021-01-13 22:18:54 -0300

    Adds my email with rights to see dashboard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\Team;
 use App\Policies\TeamPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -26,6 +27,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('viewWebSocketsDashboard', function ($user = null) {
+            return $user && in_array($user->email, [
+                'tonysm@hey.com',
+            ]);
+        });
     }
 }
