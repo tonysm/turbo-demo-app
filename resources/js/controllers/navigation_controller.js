5d8987148346718a05c3599ef280cdb5342d663d
Tony Messias <tonysm@hey.com>
2021-01-17 20:18:29 -0300

    Handle the text color on navigation

--- resources/js/controllers/navigation_controller.js
+++ resources/js/controllers/navigation_controller.js
@@ -7,9 +7,13 @@ export default class extends Controller {
         Array.from(this.element.querySelectorAll('a')).forEach((item) => {
             item.classList.remove('border-b-2');
             item.classList.remove('border-indigo-400');
+            item.classList.remove('text-gray-900');
+            item.classList.add('text-gray-500');
         });
 
+        e.target.classList.remove('text-gray-500');
         e.target.classList.add('border-b-2');
         e.target.classList.add('border-indigo-400');
+        e.target.classList.add('text-gray-900');
     }
 }
