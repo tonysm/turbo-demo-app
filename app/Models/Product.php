f7e60b2175381927bab09865fa25a428dba09971
Tony Messias <tonysm@hey.com>
2021-01-17 21:32:56 -0300

    Adds product price_for_display accessor

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Product extends Model
 {
     use HasFactory;
+
+    public function getPriceForDisplayAttribute()
+    {
+        return sprintf('$ %s', number_format($this->price_cents / 100, 2));
+    }
 }
