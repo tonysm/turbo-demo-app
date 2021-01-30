4e2b85e845ef69d906a0fa4a5fc0f8ba46fe78b5
Tony Messias <tonysm@hey.com>
2021-01-29 22:50:15 -0300

    Add the nunito font. Makes a huge difference

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,8 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
