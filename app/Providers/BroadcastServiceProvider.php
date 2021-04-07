af434ca48763f942addb52e6bbb49be6e81ec9d8
Tony Messias <tonysm@hey.com>
2021-04-07 00:10:30 -0300

    Load broadcasting channels on Octane differently

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Octane\Events\RequestReceived;
 
 class BroadcastServiceProvider extends ServiceProvider
 {
@@ -16,6 +17,17 @@ class BroadcastServiceProvider extends ServiceProvider
     {
         Broadcast::routes();
 
+        if ($_SERVER['LARAVEL_OCTANE'] ?? false) {
+            $this->app['events']->listen(RequestReceived::class, function () {
+                $this->loadChannelRoutes();
+            });
+        } else {
+            $this->loadChannelRoutes();
+        }
+    }
+
+    public function loadChannelRoutes(): void
+    {
         require base_path('routes/channels.php');
     }
 }
