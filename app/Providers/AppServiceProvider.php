4e93d4a949a697cac93b1b0a995ff1b5050b05ba
Tony Messias <tonysm@hey.com>
2021-04-06 23:05:44 -0300

    Removes forcing the HTTPS on routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Tonysm\TurboLaravel\Facades\Turbo;
 
@@ -26,9 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Turbo::broadcastToOthers(true);
-
-        if (! $this->app->environment('local')) {
-            URL::forceScheme('https');
-        }
     }
 }
