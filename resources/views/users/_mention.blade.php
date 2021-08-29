4f79ecd684f2c4e556015c08649475b48298e250
Tony Messias <tonysm@hey.com>
2021-08-29 03:06:40 -0300

    Remove underline form mention figure

--- resources/views/users/_mention.blade.php
+++ resources/views/users/_mention.blade.php
@@ -1,4 +1,4 @@
-<a class="space-x-1" data-turbo-frame="_top" href="{{ route('posts.index') }}">
+<a class="space-x-1 no-underline" data-turbo-frame="_top" href="{{ route('posts.index') }}">
     <img
         class="inline-block object-cover w-4 h-4 rounded-full"
         src="{{ $user->profile_photo_url }}"
