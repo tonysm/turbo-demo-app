3ec33c86a1ae1a507593a205488683c8638f05b9
Tony Messias <tonysm@hey.com>
2021-08-29 15:42:14 -0300

    Adds the undo/redo buttons

--- resources/views/components/trix-toolbar.blade.php
+++ resources/views/components/trix-toolbar.blade.php
@@ -1,82 +1,87 @@
 @props(['id'])
 
 <div class="sticky top-0 z-10 px-1 pt-3 -mx-1 -mt-3 bg-white border-b trix-toolbar" id="{{ $id }}">
-    <div class="flex flex-wrap items-center" data-controller="reveal" data-reveal-hide-keys-value="escape,enter">
-        <div class="flex items-center mb-3 mr-3 overflow-hidden bg-white border border-gray-300 rounded">
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="bold" data-trix-key="b" title="Bold" tabindex="-1">
-                <x-icon type="bold" />
-            </button>
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="italic" data-trix-key="i" title="Italic" tabindex="-1">
-                <x-icon type="italic" />
-            </button>
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="strike" title="Strike" tabindex="-1">
-                <x-icon type="strike" />
-            </button>
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="href" data-trix-action="link" data-action="click->reveal#toggle mousedown->reveal#toggle" data-trix-key="k"
-                title="Link" tabindex="-1">
-                <x-icon type="link" />
-            </button>
+    <div class="flex flex-wrap items-center justify-between" data-controller="reveal" data-reveal-hide-keys-value="escape,enter">
+        <div class="flex flex-wrap items-center">
+            <div class="flex items-center mb-3 mr-3 overflow-hidden bg-white border border-gray-300 rounded">
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="bold" data-trix-key="b" title="Bold" tabindex="-1">
+                    <x-icon type="bold" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="italic" data-trix-key="i" title="Italic" tabindex="-1">
+                    <x-icon type="italic" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="strike" title="Strike" tabindex="-1">
+                    <x-icon type="strike" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="href" data-trix-action="link" data-action="click->reveal#toggle mousedown->reveal#toggle" data-trix-key="k"
+                    title="Link" tabindex="-1">
+                    <x-icon type="link" />
+                </button>
+            </div>
+            <div class="flex items-center mb-3 mr-3 overflow-hidden bg-white border border-gray-300 rounded"
+                data-trix-button-group="block-tools">
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="heading1" title="Heading 1" tabindex="-1">
+                    <x-icon type="h1" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="heading2" title="Heading 2" tabindex="-1">
+                    <x-icon type="h2" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="heading3" title="Heading 3" tabindex="-1">
+                    <x-icon type="h3" />
+                </button>
+            </div>
+            <div class="flex items-center mb-3 mr-3 overflow-hidden bg-white border border-gray-300 rounded">
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="quote" title="Quotes" tabindex="-1">
+                    <x-icon type="quotes" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="code" title="Code" tabindex="-1">
+                    <x-icon type="code" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="bullet" title="Bullets" tabindex="-1">
+                    <x-icon type="bullets" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-attribute="number" title="List" tabindex="-1">
+                    <x-icon type="list" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-action="decreaseNestingLevel" title="Indent Left in a List" tabindex="-1">
+                    <x-icon type="indent-left" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-action="increaseNestingLevel" title="Indent Right in a List" tabindex="-1">
+                    <x-icon type="indent-right" />
+                </button>
+            </div>
+            <div class="flex items-center mb-3 overflow-hidden bg-white border border-gray-300 rounded">
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-action="attachFiles" title="Attach Files" tabindex="-1">
+                    <x-icon type="attachment" />
+                </button>
+            </div>
         </div>
 
-        <div class="flex items-center mb-3 mr-3 overflow-hidden bg-white border border-gray-300 rounded"
-            data-trix-button-group="block-tools">
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="heading1" title="Heading 1" tabindex="-1">
-                <x-icon type="h1" />
-            </button>
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="heading2" title="Heading 2" tabindex="-1">
-                <x-icon type="h2" />
-            </button>
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="heading3" title="Heading 3" tabindex="-1">
-                <x-icon type="h3" />
-            </button>
-        </div>
-
-        <div class="flex items-center mb-3 mr-3 overflow-hidden bg-white border border-gray-300 rounded">
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="quote" title="Quotes" tabindex="-1">
-                <x-icon type="quotes" />
-            </button>
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="code" title="Code" tabindex="-1">
-                <x-icon type="code" />
-            </button>
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="bullet" title="Bullets" tabindex="-1">
-                <x-icon type="bullets" />
-            </button>
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-attribute="number" title="List" tabindex="-1">
-                <x-icon type="list" />
-            </button>
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-action="decreaseNestingLevel" title="Indent Left in a List" tabindex="-1">
-                <x-icon type="indent-left" />
-            </button>
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-action="increaseNestingLevel" title="Indent Right in a List" tabindex="-1">
-                <x-icon type="indent-right" />
-            </button>
-        </div>
-
-        <span class="hidden trix-button-group trix-button-group--history-tools" data-trix-button-group="history-tools">
-            <button type="button" class="trix-button--icon trix-button--icon-undo" data-trix-action="undo"
-                data-trix-key="z" title="#{lang.undo}" tabindex="-1">#{lang.undo}</button>
-            <button type="button" class="trix-button--icon trix-button--icon-redo" data-trix-action="redo"
-                data-trix-key="shift+z" title="#{lang.redo}" tabindex="-1">#{lang.redo}</button>
-        </span>
-
-        <div class="flex items-center mb-3 mr-3 overflow-hidden bg-white border border-gray-300 rounded">
-            <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
-                data-trix-action="attachFiles" title="Attach Files" tabindex="-1">
-                <x-icon type="attachment" />
-            </button>
+        <div>
+            <div class="flex items-center mb-3 mr-3 overflow-hidden bg-white border border-gray-300 rounded" data-trix-button-group="history-tools">
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-action="undo" data-trix-key="z" title="Undo" tabindex="-1">
+                    <x-icon type="undo" />
+                </button>
+                <button type="button" class="flex items-center justify-center text-gray-700 w-9 h-9"
+                    data-trix-action="redo" data-trix-key="shift+z" title="Redo" tabindex="-1">
+                    <x-icon type="redo" />
+                </button>
+            </div>
         </div>
 
         <div hidden data-reveal data-trix-dialogs>
