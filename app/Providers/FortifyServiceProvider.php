d9ebcd22a2e68c1e2266e57a1ddb2a30743772a1
Tony Messias <tonysm@hey.com>
2021-08-29 21:27:38 -0300

    Adds back the two-factor rate limiter

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -39,5 +39,9 @@ class FortifyServiceProvider extends ServiceProvider
         RateLimiter::for('login', function (Request $request) {
             return Limit::perMinute(5)->by($request->email.$request->ip());
         });
+
+        RateLimiter::for('two-factor', function (Request $request) {
+            return Limit::perMinute(5)->by($request->session()->get('login.id'));
+        });
     }
 }
