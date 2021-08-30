3140ef499fd7469e62111e985779c57342aa0d51
Tony Messias <tonysm@hey.com>
2021-08-29 22:53:11 -0300

    Remove octane

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Broadcast;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Octane\Events\RequestReceived;
 
 class BroadcastServiceProvider extends ServiceProvider
 {
@@ -16,14 +15,6 @@ class BroadcastServiceProvider extends ServiceProvider
     public function boot()
     {
         Broadcast::routes();
-
-        if ($_SERVER['LARAVEL_OCTANE'] ?? false) {
-            $this->app['events']->listen(RequestReceived::class, function () {
-                $this->loadChannelRoutes();
-            });
-        } else {
-            $this->loadChannelRoutes();
-        }
     }
 
     public function loadChannelRoutes(): void
