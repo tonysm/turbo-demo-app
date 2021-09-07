ba0a9d0bddc1dbbe49ba1e672e736539af8c179b
Tony Messias <tonysm@hey.com>
2021-09-06 22:38:53 -0300

    Uses custom polymorphic types

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         // Turbo::broadcastToOthers(true);
+        Relation::morphMap([
+            'user' => User::class,
+        ]);
     }
 }
