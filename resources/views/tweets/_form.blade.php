9893f5c7ef99a113fd071e08ec9244751a41891f
Tony Messias <tonysm@hey.com>
2021-09-15 20:44:08 -0300

    Tweaks the tweets form actions

--- resources/views/tweets/_form.blade.php
+++ resources/views/tweets/_form.blade.php
@@ -1,6 +1,6 @@
-<div class="bg-white p-4 space-x-4 flex">
+<div class="flex p-4 space-x-4 bg-white">
     <div>
-        <img class="h-8 w-8 rounded-full object-cover" src="{{ ($tweet->user ?: auth()->user())->profile_photo_url }}" alt="{{ ($tweet->user ?: auth()->user())->name }}" />
+        <img class="object-cover w-8 h-8 rounded-full" src="{{ ($tweet->user ?: auth()->user())->profile_photo_url }}" alt="{{ ($tweet->user ?: auth()->user())->name }}" />
     </div>
     <form
         class="flex-1"
@@ -18,13 +18,13 @@
 
         <div class="block">
             <label for="content" class="sr-only">Tweet</label>
-            <textarea class="form-textarea w-full" name="content" id="" rows="2" placeholder="What's happening?">{{ old('content', $tweet->content) }}</textarea>
+            <textarea class="w-full form-textarea" name="content" id="" rows="2" placeholder="What's happening?">{{ old('content', $tweet->content) }}</textarea>
             @error('content')
             <span class="mt-2 text-sm text-red-600">{{ $message }}</span>
             @enderror
         </div>
 
-        <div class="sm:flex sm:justify-between sm:items-center space-y-4 sm:space-y-0">
+        <div class="mt-1 space-y-4 sm:flex sm:justify-between sm:items-center sm:space-y-0">
             <div class="flex items-center space-x-4 text-blue-600">
                 <a href="#">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
@@ -41,14 +41,14 @@
             </div>
             <div class="space-x-1">
                 @if($tweet->exists)
-                    <a x-data @click="$dispatch('close')" href="{{ route('tweets.show', $tweet) }}" class="px-4 py-2 text-gray-500 hover:bg-gray-100 font-semibold rounded-full text-sm">
+                    <a x-data @click="$dispatch('close')" href="{{ route('tweets.show', $tweet) }}" class="px-4 py-2 text-sm font-semibold text-gray-500 rounded-full hover:bg-gray-100">
                         Cancel
                     </a>
-                    <button class="px-4 py-2 bg-blue-600 text-white font-semibold rounded-full text-sm">
+                    <button class="px-4 py-2 text-sm font-semibold text-white bg-blue-600 rounded-full">
                         Update
                     </button>
                 @else
-                    <button class="px-4 py-2 bg-blue-600 text-white font-semibold rounded-full text-sm">
+                    <button class="px-4 py-2 text-sm font-semibold text-white bg-blue-600 rounded-full">
                         Tweet
                     </button>
                 @endif
