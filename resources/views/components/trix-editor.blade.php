72a1a836c8c5711e16c05eea84b231e2978c23f2
Tony Messias <tonysm@hey.com>
2021-09-15 20:21:55 -0300

    Tweaks the trix-editor layout

--- resources/views/components/trix-editor.blade.php
+++ resources/views/components/trix-editor.blade.php
@@ -1,12 +1,12 @@
 @props(['id' => '', 'toolbar' => ''])
 
-<div class="relative">
+<div class="relative border border-gray-300 rounded-md">
     <x-trix-toolbar id="{{ $id }}_toolbar" />
 
     <input id="{{ $id }}" {{ $attributes->except(['class', 'style']) }} type="hidden">
 
     <trix-editor input="{{ $id }}" toolbar="{{ $id }}_toolbar"
-        {{ $attributes->merge(['style' => '', 'class' => 'trix-content form-input mt-2']) }} data-controller="trix"
+        {{ $attributes->merge(['style' => '', 'class' => 'p-4 trix-content border-0 mt-2']) }} data-controller="trix"
         data-action="
             click->trix#prevent
             trix-attachment-add->trix#upload
