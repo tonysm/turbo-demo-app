612d6e6faceda1644f59bc7d4dc5f2cc2a2d0fd7
Tony Messias <tonysm@hey.com>
2021-11-13 15:02:23 -0300

    Only declares dom_id fn if it does not exist

--- app/helpers.php
+++ app/helpers.php
@@ -2,7 +2,9 @@
 
 use function Tonysm\TurboLaravel\dom_id as turbo_laravel_dom_id;
 
-function dom_id(...$args)
-{
-    return turbo_laravel_dom_id(...$args);
+if (! function_exists('dom_id')) {
+    function dom_id(...$args)
+    {
+        return turbo_laravel_dom_id(...$args);
+    }
 }
