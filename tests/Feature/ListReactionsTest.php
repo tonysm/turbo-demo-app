33a213f347e20a67fa57d28de92033e819f016ba
Tony Messias <tonysm@hey.com>
2021-11-24 14:53:18 -0300

    Adds list reactions test

---
+++ tests/Feature/ListReactionsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ListReactionsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function must_be_authenticated()
+    {
+    }
+
+    /** @test */
+    public function must_be_from_same_team()
+    {
+    }
+
+    /** @test */
+    public function can_list_reactions_of_an_entry()
+    {
+    }
+}
