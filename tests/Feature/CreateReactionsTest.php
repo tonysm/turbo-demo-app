152c62396ee667cea93bae43715763c1b6f3d1fe
Tony Messias <tonysm@hey.com>
2021-11-24 12:14:52 -0300

    Adds the create reactions test

---
+++ tests/Feature/CreateReactionsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateReactionsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function must_be_authenticated()
+    {
+    }
+
+    /** @test */
+    public function must_be_from_same_team()
+    {
+    }
+
+    /** @test */
+    public function can_create_reaction()
+    {
+    }
+}
