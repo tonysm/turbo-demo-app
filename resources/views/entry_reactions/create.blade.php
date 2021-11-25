7a2e89f7c6a6c500028255e29afa13146cac1e97
Tony Messias <tonysm@hey.com>
2021-11-25 16:22:28 -0300

    Make search form sticky

--- resources/views/entry_reactions/create.blade.php
+++ resources/views/entry_reactions/create.blade.php
@@ -13,23 +13,24 @@
         <div class="h-full mx-auto space-y-12 max-w-7xl sm:px-6 lg:px-8">
             <div class="h-full p-2 bg-white rounded-lg shadow md:p-8 lg:p-16">
                 <turbo-frame id="@domid($entry, 'create_reaction')" target="_top">
-                    <form x-data @click.stop action="{{ route('entries.reactions.create', $entry) }}" method="GET" data-turbo-frame="@domid($entry, 'create_reaction')">
-                        <x-jet-label class="sr-only">Type to search...</x-jet-label>
-                        <x-jet-input name="search" value="{{ old('search', request('search')) }}" placeholder="Type to search..." class="w-full p-2 my-2 text-base border rounded-full sm:border-transparent" autofocus autocomplete="off" />
-                        <button type="submit" hidden>Submit</button>
-                    </form>
-
-                    <div class="grid grid-cols-7 gap-2 pt-2 mt-4 overflow-y-auto border-t sm:max-h-40 group-busy:opacity-50" x-data x-on:click.stop>
-                        @forelse ($emojis as $emoji)
-                            <form x-data x-on:turbo:submit-end="$dispatch('close')" action="{{ route('entries.reactions.store', $entry) }}" method="POST" class="block mx-auto">
-                                <input type="hidden" name="emoji" value="{{ $emoji['short_name'] }}" />
-                                <button class="p-2 bg-white border rounded-full" title="React with {{ $emoji['short_name'] }}">
-                                    <x-emoji :name="$emoji['short_name']" />
-                                </button>
-                            </form>
-                        @empty
-                            <p class="text-center text-gray-400">No emojis found.</p>
-                        @endforelse
+                    <div class="relative">
+                        <form class="sticky top-0" x-data @click.stop action="{{ route('entries.reactions.create', $entry) }}" method="GET" data-turbo-frame="@domid($entry, 'create_reaction')">
+                            <x-jet-label class="sr-only">Type to search...</x-jet-label>
+                            <x-jet-input name="search" value="{{ old('search', request('search')) }}" placeholder="Type to search..." class="w-full p-2 my-2 text-base border rounded-full sm:border-transparent" autofocus autocomplete="off" />
+                            <button type="submit" hidden>Submit</button>
+                        </form>
+                        <div class="grid grid-cols-7 gap-2 pt-2 mt-2 overflow-y-auto border-t sm:max-h-40 group-busy:opacity-50" x-data x-on:click.stop>
+                            @forelse ($emojis as $emoji)
+                                <form x-data x-on:turbo:submit-end="$dispatch('close')" action="{{ route('entries.reactions.store', $entry) }}" method="POST" class="block mx-auto">
+                                    <input type="hidden" name="emoji" value="{{ $emoji['short_name'] }}" />
+                                    <button class="p-2 bg-white border rounded-full" title="React with {{ $emoji['short_name'] }}">
+                                        <x-emoji :name="$emoji['short_name']" />
+                                    </button>
+                                </form>
+                            @empty
+                                <p class="text-center text-gray-400">No emojis found.</p>
+                            @endforelse
+                        </div>
                     </div>
                 </turbo-frame>
             </div>
