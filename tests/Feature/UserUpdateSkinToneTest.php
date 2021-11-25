ba2f446b9ddc40dfc7b1e5085714d4563f5d25d5
Tony Messias <tonysm@hey.com>
2021-11-25 16:24:18 -0300

    Adds skin tone update tests

---
+++ tests/Feature/UserUpdateSkinToneTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class UserUpdateSkinToneTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function must_be_authenticated()
+    {
+    }
+
+    /** @test */
+    public function must_be_valid_skin_tone()
+    {
+    }
+
+    /** @test */
+    public function can_update_skin_tone()
+    {
+
+    }
+}
