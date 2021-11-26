45a30c3bb6d4d759e5e05c192f9639390184eb0d
Tony Messias <tonysm@hey.com>
2021-11-26 19:07:44 -0300

    Tweaks posts prunning messages

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -17,7 +17,7 @@
 
     <div class="py-2 md:py-12">
         <div class="mx-auto max-w-7xl sm:px-8 lg:px-18">
-            <p class="hidden p-2 mx-4 text-sm text-center text-gray-900 border rounded-full shadow-sm bg-yellow-50 md:block">
+            <p class="hidden p-2 mx-4 text-sm text-center text-gray-900 border rounded shadow-sm bg-yellow-50 md:block">
                 ⚠️ Since this is a demo app, Posts and Comments will automatically be deleted after a couple of days. ⚠️
             </p>
 
