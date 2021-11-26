360f38db2522612bf582268556537615bbd40f9f
Tony Messias <tonysm@hey.com>
2021-11-26 19:04:22 -0300

    Prunes posts for everyone

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -90,7 +90,7 @@ class Post extends Model
 
     public function prunable()
     {
-        return static::where('created_at', '<=', now()->subWeek())->where('team_id', 1)->take(1);
+        return static::where('created_at', '<=', now()->subDays(2));
     }
 
     public function pruning()
