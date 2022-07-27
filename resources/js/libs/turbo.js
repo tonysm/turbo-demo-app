09974645b66f42713f159499e1faec02455f72a8
Tony Messias <tonysm@hey.com>
2022-07-26 23:28:36 -0300

    No need to manually set the csrf token since Turbo already handles it

--- resources/js/libs/turbo.js
+++ resources/js/libs/turbo.js
@@ -4,18 +4,18 @@ window.Turbo = Turbo;
 
 Turbo.start();
 
-const registerTurboRequestInterceptorToAddCsrfToken = () => {
-    let token = document.head.querySelector('meta[name="csrf-token"]');
+// const registerTurboRequestInterceptorToAddCsrfToken = () => {
+//     let token = document.head.querySelector('meta[name="csrf-token"]');
 
-    document.addEventListener('turbo:before-fetch-request', (e) => {
-        if (token) {
-            e.detail.fetchOptions.headers['X-CSRF-Token'] = token.content;
-        } else {
-            console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
-        }
-    });
-}
+//     document.addEventListener('turbo:before-fetch-request', (e) => {
+//         if (token) {
+//             e.detail.fetchOptions.headers['X-CSRF-Token'] = token.content;
+//         } else {
+//             console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+//         }
+//     });
+// }
 
-registerTurboRequestInterceptorToAddCsrfToken();
+// registerTurboRequestInterceptorToAddCsrfToken();
 
 export default Turbo;
